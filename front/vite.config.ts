99bb9b693b0dea9203cc389eebcdb0c9622eb070
jehee826 <jehee826@gmail.com>
2026-06-01 12:03:33 +0900

    IP 전역변수 파일 추가

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ip from './default.ts';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
     proxy: {
       // 프론트엔드에서 '/api'로 시작하는 요청을 보내면 백엔드(8080)로 전달합니다.
       '/api': {
-        target: 'http://localhost:8080',
+        target: `http://${ip}:8080`,
+        
         changeOrigin: true,
         // 주소에서 '/api'를 제거하고 백엔드에 전달하고 싶다면 아래 주석 해제
         // rewrite: (path) => path.replace(/^\/api/, '')
