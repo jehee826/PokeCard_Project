7eb05652a13f689c48a28622036c485eef18cbf8
jehee826 <jehee826@gmail.com>
2026-06-11 21:03:16 +0900

    파일정리

--- front/default.ts
+++ front/default.ts
@@ -1,6 +1,6 @@
 const ip = "localhost"; //기본 게이트웨이
 // const ip = "10.32.7.27"; //준호꺼(핫스팟)
-//const ip = "192.168.0.6" //집컴(로컬)
+// const ip = "192.168.0.6" //집컴(로컬)
 // const ip = "10.32.7.120" //내꺼(핫스팟)
 
 export default ip;
